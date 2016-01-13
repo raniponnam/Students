1247bf9b5013a30e7c8b3498b3539ddf4db856e9
raniponnam <raniponnam83@gmail.com>
2016-01-12 19:19:08 -0800

    I changed config name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
  config.assets.compile = true
 
